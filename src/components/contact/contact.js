7531d519418afa70e3b29d7cebf8730b00899a68
Anthony Ingall <tonyingall.business@gmail.com>
2020-05-22 23:09:06 +0100

    Contact form fix URL encoding added to contact form component to comply with Netlify 	modified:   src/components/contact/contact.js

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -3,45 +3,64 @@ import validate from "./validators"
 import { Fade } from "react-reveal"
 
 export default function Contact() {
+ 
+  let form = React.createRef();
 
-  const [{ mail, name, subject, message }, setFormValues] = useState({
+  const encode = (data) => {
+      return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&")
+    }
+
+  const [pretendState, setFormValues] = useState({
     mail: "",
     name: "",
     subject: "",
     message: "",
   })
   const [loading, setLoading] = useState(false)
-
+ 
   function handleChange({ target: { value, name } }) {
     setFormValues(previousValues => ({ ...previousValues, [name]: value }))
   }
-
+ 
   async function handleSubmit(event) {
     event.preventDefault();
+    const netlifyForm = form.current;
     setLoading(true);
-    
+   
     await fetch('/', {
       method: 'POST',
-      body: new URLSearchParams({name: name, mail: mail, subject: subject, message: message}).toString()
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+          "form-name": netlifyForm.getAttribute("name"),
+          ...pretendState
+        }),
+      // body: new URLSearchParams({name: name, mail: mail, subject: subject, message: message}).toString()
     });
-  
+ 
     alert('thank you!');
-  
+ 
     setLoading(false);
   }
-
-  const isValidMail = validate.mail(mail)
-  const isValidName = validate.name(name)
-  const isValidSubject = validate.subject(subject)
-  const isValidMessage = validate.message(message)
-
+ 
+  const isValidMail = validate.mail(pretendState.mail)
+  const isValidName = validate.name(pretendState.name)
+  const isValidSubject = validate.subject(pretendState.subject)
+  const isValidMessage = validate.message(pretendState.message)
+ 
   // this currently expects the mail to be a valid mail and every other variable to not be empty
   const isValidForm = isValidMail && isValidName && isValidSubject && isValidMessage
-
+ 
   return (
-    <form name="contact" onSubmit={handleSubmit} autoComplete="off" className="form" netlify data-netlify="true" netlify-honeypot="bot-field">
+    <form ref={form} name="contact" onSubmit={handleSubmit} method="POST" autoComplete="off" className="form" netlify data-netlify="true" netlify-honeypot="bot-field">
+      <input type="hidden" name="form-name" value="contact" />
+      <div hidden>
+          <label>
+            Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+          </label>
+      </div>
       <fieldset disabled={loading}>
-        <input type="hidden" name="form-name" value="contact" />
         <label for="name" htmlFor="name">Name</label>
         <input
           id="name"
@@ -50,9 +69,9 @@ export default function Contact() {
           type="text"
           placeholder="Jane Doe..."
           required
-          className={name.length > 2 && !isValidName ? "error" : ""}
+          className={pretendState.name.length > 2 && !isValidName ? "error" : ""}
         />
-        <Fade left collapse when={name.length > 2 && !isValidName}>
+        <Fade left collapse when={pretendState.name.length > 2 && !isValidName}>
           <p className="error-message">Could you tell me your full name?</p>
         </Fade>
         <label name="mail" htmlFor="mail">Email</label>
@@ -62,10 +81,10 @@ export default function Contact() {
           onChange={handleChange}
           type="text"
           placeholder="janedoe@doemail.com..."
-          className={mail.length > 2 && !isValidMail ? "error" : ""}
+          className={pretendState.mail.length > 2 && !isValidMail ? "error" : ""}
           required
         />
-        <Fade left collapse when={mail.length > 2 && !isValidMail}>
+        <Fade left collapse when={pretendState.mail.length > 2 && !isValidMail}>
           <p className="error-message">Could you give me a full email address?</p>
         </Fade>
         <label for="subject" htmlFor="subject">Subject</label>
@@ -75,10 +94,10 @@ export default function Contact() {
           onChange={handleChange}
           type="text"
           placeholder="Project idea..."
-          className={subject.length > 2 && !isValidSubject ? "error" : ""}
+          className={pretendState.subject.length > 2 && !isValidSubject ? "error" : ""}
           required
         />
-        <Fade left collapse when={subject.length > 2 && !isValidSubject}>
+        <Fade left collapse when={pretendState.subject.length > 2 && !isValidSubject}>
           <p className="error-message">Please type a longer subject</p>
         </Fade>
         <label for="message" htmlFor="message">Message</label>
@@ -87,11 +106,11 @@ export default function Contact() {
           name="message"
           onChange={handleChange}
           placeholder="My project idea is..."
-          className={message.length > 2 && !isValidMessage ? "error" : ""}
+          className={pretendState.message.length > 2 && !isValidMessage ? "error" : ""}
           required
         />
-        <p className={message.length > 0 && !isValidMessage ? "character-limit error" : "character-limit"}>{message.length}/150</p>
-        <Fade left collapse when={message.length > 2 && !isValidMessage}>
+        <p className={pretendState.message.length > 0 && !isValidMessage ? "character-limit error" : "character-limit"}>{pretendState.message.length}/150</p>
+        <Fade left collapse when={pretendState.message.length > 2 && !isValidMessage}>
           <p className="error-message">Please make sure the message is more than 150 characters</p>
         </Fade>
         <button
